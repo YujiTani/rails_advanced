8eb9ffb2c0c5f91b072bdeadeeb20dc090aff9fa
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-07-05 07:57:09 +0900

    公開ボタンを押した際の挙動を修正

--- app/controllers/admin/articles/publishes_controller.rb
+++ app/controllers/admin/articles/publishes_controller.rb
@@ -4,19 +4,30 @@ class Admin::Articles::PublishesController < ApplicationController
   before_action :set_article
 
   def update
-    @article.published_at = Time.current unless @article.published_at?
-    @article.state = :published
+    # 現在時刻Dataオブジェクトを取得
+    current_time = Time.current
+
+    @article.published_at = current_time unless @article.published_at?
+    # 時差によるバグ防止の為、UTCで比較
+    @article.state = if @article.state == 'published' || @article.state == 'publish_wait'
+                       current_time.utc.to_f >= @article.published_at.utc.to_f ? :published : :publish_wait
+                     end
 
     if @article.valid?
+      # トランザクションを実行
       Article.transaction do
-        @article.body = @article.build_body(self)
-        @article.save!
+        @article.update!(
+          body: @article.build_body(self),
+          published_at: @article.published_at,
+          state: @article.state
+        )
       end
 
-      flash[:notice] = '記事を公開しました'
+      flash[:notice] = @article.state == 'published' ? '記事を公開しました' : '公開待ちにしました'
 
       redirect_to edit_admin_article_path(@article.uuid)
     else
+
       flash.now[:alert] = 'エラーがあります。確認してください。'
 
       @article.state = @article.state_was if @article.state_changed?
