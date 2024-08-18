1629a6cb5b573d6e64c7de15e516734c98cb2f88
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-08-18 23:19:34 +0900

    modelのscope設定を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -62,7 +62,10 @@ class Article < ApplicationRecord
   scope :viewable, -> { published.where('published_at < ?', Time.current) }
   scope :new_arrivals, -> { viewable.order(published_at: :desc) }
   scope :by_category, ->(category_id) { where(category_id: category_id) }
+  scope :by_author, ->(author_id) { where(author_id: author_id) }
   scope :title_contain, ->(word) { where('title LIKE ?', "%#{word}%") }
+  scope :body_contain, ->(word) { where('body LIKE ?', "%#{word}%") }
+  scope :with_tag, ->(tag_id) { joins(:tags).where(tags: { id: tag_id }) }
 
   def build_body(controller)
     result = ''
