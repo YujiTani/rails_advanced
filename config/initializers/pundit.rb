fa1d1bacbb58a9a213717af1f305436c8c7a5181
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-08-24 07:03:21 +0900

    エラー時の対応レベルを変更

--- config/initializers/pundit.rb
+++ config/initializers/pundit.rb
@@ -1,1 +1,5 @@
-Rails.application.config.action_dispatch.rescue_responses['Pundit::NotAuthorizedError'] = :not_found
+# Rails.application.config.action_dispatch.rescue_responses['Pundit::NotAuthorizedError'] = :not_found
+
+#例外を403HTTPステータスにします。これを付けないと500になる｡
+# :forbiddenというシンボルはステータスコード403と定義されている｡
+Rails.application.config.action_dispatch.rescue_responses["Pundit::NotAuthorizedError"] = :forbidden
